fa99af3e5b807ef5da57b02799149b1295bcf7d1
albertlatacz <albert.latacz@gmail.com>
2021-03-06 12:43:28 +0000

    derive correct project version

--- scripts/set-environment.sh
+++ scripts/set-environment.sh
@@ -19,7 +19,7 @@ PROJECT_NAME="Gradle S3 Build Cache"
 PROJECT_ID="gradle-s3-build-cache"
 PROJECT_OWNER="talk2duck"
 PROJECT_ROOT_DIR=$(realpath "$SCRIPTS_DIR/..")
-PROJECT_VERSION=$($JQ -r ".[\"@${PROJECT_ID}\"].version" version.json)
+PROJECT_VERSION=$($JQ -r ".[\"@${PROJECT_ID}\"].version" "$PROJECT_ROOT_DIR/version.json")
 BINTRAY_LATEST_VERSION_URL="https://bintray.com/api/v1/packages/${PROJECT_OWNER}/maven/${PROJECT_ID}/versions/_latest"
 
 
