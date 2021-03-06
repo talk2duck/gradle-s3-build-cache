0b69155574c6017eb94db4e56375b6973a82fe0a
albertlatacz <albert.latacz@gmail.com>
2021-03-06 15:10:40 +0000

    added publishing to maven central staging

---
+++ scripts/release-publish-maven-central-staging.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -o errexit
+set -o pipefail
+set -o nounset
+
+SET_ENV_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "$SET_ENV_DIR/set-environment.sh"
+
+cd "$PROJECT_ROOT_DIR" || exit 1
+
+./gradlew publishMavenJavaPublicationToSonatypeStagingRepository \
+  -PsigningKey="${SIGNING_KEY}" \
+  -PsigningPassword="${SIGNING_PASSWORD}" \
+  -PnexusUsername="${NEXUS_USERNAME}" \
+  -PnexusPassword="${NEXUS_PASSWORD}"
