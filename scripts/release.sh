a1f4db53fdcc978cd5b6a7954dc4c0d1dff6a811
albertlatacz <albert.latacz@gmail.com>
2021-03-06 12:58:18 +0000

    fixed version and added print project info

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,6 +7,7 @@ set -o nounset
 
 SET_ENV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$SET_ENV_DIR/set-environment.sh"
+printProjectInfo
 
 cd "$PROJECT_ROOT_DIR" || exit 1
 
--- scripts/set-environment.sh
+++ scripts/set-environment.sh
@@ -5,22 +5,25 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 TOOLS_DIR="$SCRIPTS_DIR/tools"
 
-if ! command -v jq &> /dev/null
-then
-    JQ="$TOOLS_DIR/jq"
+if ! command -v jq &>/dev/null; then
+  JQ="$TOOLS_DIR/jq"
 else
-    JQ=jq
+  JQ=jq
 fi
+
+function printProjectInfo() {
+  echo "Project (${PROJECT_ID}):"
+  echo "  Directory: ${PROJECT_ROOT_DIR} "
+  echo "  Version: ${PROJECT_VERSION} "
+  echo
+}
 
 PROJECT_NAME="Gradle S3 Build Cache"
 PROJECT_ID="gradle-s3-build-cache"
 PROJECT_OWNER="talk2duck"
 PROJECT_ROOT_DIR=$(realpath "$SCRIPTS_DIR/..")
-PROJECT_VERSION=$($JQ -r ".[\"@${PROJECT_ID}\"].version" "$PROJECT_ROOT_DIR/version.json")
+PROJECT_VERSION=$($JQ -r ".project.version" "$PROJECT_ROOT_DIR/version.json")
 BINTRAY_LATEST_VERSION_URL="https://bintray.com/api/v1/packages/${PROJECT_OWNER}/maven/${PROJECT_ID}/versions/_latest"
-
-
-
