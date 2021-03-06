afe67c07d49c99ce6e712e138379d8ce8fc3fe4d
albertlatacz <albert.latacz@gmail.com>
2021-03-06 09:23:43 +0000

    release script that pushes to git

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,6 +8,8 @@ set -o nounset
 SET_ENV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$SET_ENV_DIR/set-environment.sh"
 
+cd "$PROJECT_ROOT_DIR" || exit 1
+
 NEW_VERSION=${1:-}
 
 if [ "$NEW_VERSION" == "" ]; then
@@ -17,25 +19,21 @@ fi
 
 echo "Creating release $PROJECT_NAME:$NEW_VERSION"
 
-#git tag -a "$NEW_VERSION" -m "$PROJECT_NAME $NEW_VERSION"
-#git push origin "$NEW_VERSION"
-
-#git stash
+git stash
 
 BINTRAY_VERSION=$(curl -s "$BINTRAY_LATEST_VERSION_URL" | "$TOOLS_DIR"/jq -r .name)
 
-ls -al "$PROJECT_ROOT_DIR/VERSION"
+sed -i -- s/"$BINTRAY_VERSION"/"$NEW_VERSION"/g "README.md"
+sed -i -- s/"$BINTRAY_VERSION"/"$NEW_VERSION"/g "version.json"
 
-sed -i -- s/"$BINTRAY_VERSION"/"$NEW_VERSION"/g "$PROJECT_ROOT_DIR/README.md"
-sed -i -- s/"$BINTRAY_VERSION"/"$NEW_VERSION"/g "$PROJECT_ROOT_DIR/VERSION"
+git add "README.md"
+git add "version.json"
 
-#git add README.md
-#
-#git commit -am"Release $NEW_VERSION"
-#
-#git push origin
-#
-#git push
-#
-#git stash apply
+git commit -am"Release $NEW_VERSION"
+
+git push origin
+
+git push
+
+git stash apply
 
