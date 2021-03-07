6984cc0f90e89416c002f936577322e44a75dd12
albertlatacz <albert.latacz@gmail.com>
2021-03-07 06:13:48 +0000

    added github release plugin

--- scripts/release-publish-github.sh
+++ scripts/release-publish-github.sh
@@ -8,21 +8,6 @@ set -o nounset
 SET_ENV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$SET_ENV_DIR/set-environment.sh"
 
-export PUBLISH_REQUEST_BODY="$(mktemp)"
+cd "$PROJECT_ROOT_DIR" || exit 1
 
-cat >"${PUBLISH_REQUEST_BODY}" <<EOL
-{
-  "tag_name": "${PROJECT_VERSION}",
-  "target_commitish": "main",
-  "name": "${PROJECT_VERSION}",
-  "body": "Released ${PROJECT_NAME} version ${PROJECT_VERSION}",
-  "draft": false,
-  "prerelease": false
-}
-EOL
-
-curl -v -i -X POST \
-    -H "Content-Type:application/json" \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
-    "https://api.github.com/repos/${PROJECT_OWNER}/${PROJECT_ID}/releases" \
-    -d "@${PUBLISH_REQUEST_BODY}"
+./gradlew githubRelease
