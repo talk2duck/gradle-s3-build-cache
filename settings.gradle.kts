2ad33b47a201173d30d159a78927613b228934ef
albertlatacz <albert.latacz@gmail.com>
2023-03-22 08:16:06 +0000

    added refresh versions

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("de.fayard.refreshVersions") version "0.51.0"
+}
+
+refreshVersions {
+    rejectVersionIf {
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
+}
