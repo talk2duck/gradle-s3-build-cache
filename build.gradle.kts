89a5dd20418d7aa9936045edb7c7f109b41766d1
Albert Latacz <albert.latacz@gmail.com>
2024-10-04 12:27:07 +0100

    removed plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
         classpath("org.jetbrains.kotlin:kotlin-serialization:2.0.20")
         classpath("io.codearte.gradle.nexus:gradle-nexus-staging-plugin:0.30.0")
-        classpath("org.openapitools:openapi-generator-gradle-plugin:6.4.0")
         classpath("com.gradle.publish:plugin-publish-plugin:1.3.0")
         classpath("com.github.breadmoirai:github-release:2.5.2")
     }
